722167b3207ad2840dbf17b78958b5f8b0feece3
ebenp <eben.pendleton@gmail.com>
2017-05-18 19:38:43 -0400

    added initial code

--- scraper.py
+++ scraper.py
@@ -1,18 +1,21 @@
 # This is a template for a Python scraper on morph.io (https://morph.io)
 # including some code snippets below that you should find helpful
 
-# import scraperwiki
+import scraperwiki
 # import lxml.html
 #
 # # Read in a page
-# html = scraperwiki.scrape("http://foo.com")
+html = scraperwiki.scrape("https://www1.ncdc.noaa.gov/pub/data/15min_precip-3260")
 #
 # # Find something on the page using css selectors
 # root = lxml.html.fromstring(html)
 # root.cssselect("div[align='left']")
 #
 # # Write out to the sqlite database using scraperwiki library
-# scraperwiki.sqlite.save(unique_keys=['name'], data={"name": "susan", "occupation": "software developer"})
+scraperwiki.sqlite.save(unique_keys=['name'], data={"name": "eben pendleton", "occupation": " environmental scientist"})
+
+page = scraperwiki.scrape('http://python.org/downloads')
+html = lxml.html.fromstring(page)
 #
 # # An arbitrary query against the database
 # scraperwiki.sql.select("* from data where 'name'='peter'")
