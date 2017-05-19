78a7e16ef5f63cf818372806252d7f1eca25bb72
ebenp <eben.pendleton@gmail.com>
2017-05-18 20:26:46 -0400

    updated urllib2

--- scraper.py
+++ scraper.py
@@ -3,7 +3,7 @@
 
 import scraperwiki
 from datetime import datetime
-from urllib2.request import Request, urlopen, urlretrieve
+from urllib2 import Request, urlopen, urlretrieve
 from bs4 import BeautifulSoup
 # import lxml.html
 #
