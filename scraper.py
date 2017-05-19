b3e451d991388bec2a1255dc7812650959a9b3ba
ebenp <eben.pendleton@gmail.com>
2017-05-18 20:43:57 -0400

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -16,22 +16,20 @@ from bs4 import BeautifulSoup
 # called "data.sqlite" in the current working directory which has at least a table
 # called "data".
 
+def listFD(url, ext=''):
+    page = requests.get(url).text
+    soup = BeautifulSoup(page, 'html.parser')
+    return [url + '/' + node.get('href') for node in soup.find_all('a') if node.get('href').endswith(ext)]
+
+
+
+
 def read_url(url):
   # Write out to the sqlite database using scraperwiki library
   todays_date = str(datetime.now())
   scraperwiki.sqlite.save(unique_keys=['name'], data={"name": "eben pendleton", "occupation": " environmental scientist"})
-  url = url.replace(" ","%20")
-  req = Request(url)
-  a = urlopen(req).read()
-  soup = BeautifulSoup(a, 'html.parser')
-  x = (soup.find_all('a'))
-  for i in x:
-      file_name = i.extract().get_text()
-      url_new = url + file_name
-      url_new = url_new.replace(" ","%20")
-      if(file_name[-1]=='/' and file_name[0]!='.'):
-          read_url(url_new)
-      print(file_name,todays_date,url_new)
+  for file_name in listFD(url, ext):
+      print(file_name,todays_date)
       #scraperwiki.sqlite.save(unique_keys=[file_name], data={"f": file_name, "d": todays_date })
 if __name__ == '__main__':
   url ="https://www1.ncdc.noaa.gov/pub/data/15min_precip-3260"
