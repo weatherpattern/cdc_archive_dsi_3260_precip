7a3d82603a59b92859343ce39b3296bb500c42f0
ebenp <eben.pendleton@gmail.com>
2017-05-18 20:10:45 -0400

    updated code

--- scraper.py
+++ scraper.py
@@ -2,20 +2,9 @@
 # including some code snippets below that you should find helpful
 
 import scraperwiki
+from ftplib import FTP
+from datetime import datetime
 # import lxml.html
-#
-# # Read in a page
-html = scraperwiki.scrape("https://www1.ncdc.noaa.gov/pub/data/15min_precip-3260")
-#
-# # Find something on the page using css selectors
-# root = lxml.html.fromstring(html)
-# root.cssselect("div[align='left']")
-#
-# # Write out to the sqlite database using scraperwiki library
-scraperwiki.sqlite.save(unique_keys=['name'], data={"name": "eben pendleton", "occupation": " environmental scientist"})
-
-page = scraperwiki.scrape('http://python.org/downloads')
-html = lxml.html.fromstring(page)
 #
 # # An arbitrary query against the database
 # scraperwiki.sql.select("* from data where 'name'='peter'")
@@ -25,3 +14,23 @@ html = lxml.html.fromstring(page)
 # All that matters is that your final data is written to an SQLite database
 # called "data.sqlite" in the current working directory which has at least a table
 # called "data".
+
+def main(url):
+  # Write out to the sqlite database using scraperwiki library
+  scraperwiki.sqlite.save(unique_keys=['name'], data={"name": "eben pendleton", "occupation": " environmental scientist"})
+  f = FTP(url)
+  f.connect("localhost")
+  f.login()
+  ls = []
+  f.retrlines('MLSD', ls.append)
+  todays_date = str(datetime.now())
+  for entry in ls:
+      scraperwiki.sqlite.save(unique_keys=[entry], data={"f": entry, "d": todays_date })
+if __name__ == '__main__':
+  url ="https://www1.ncdc.noaa.gov/pub/data/15min_precip-3260"
+  #
+  # # Find something on the page using css selectors
+  # root = lxml.html.fromstring(html)
+  # root.cssselect("div[align='left']")
+  #
+  main(url)
